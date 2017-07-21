9a4a6f9ba2b2ad81019ea3fbf9ebc8f8b6447219
Dedy Kuncoro <dedy.kuncoro.07@gmail.com>
2017-07-21 18:55:33 +0700

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# VideoStreaming
+# Video Streaming using VideoView Android
+
+Tutorial
+https://dedykuncoro.com/2017/07/tutorial-membuat-aplikasi-android-video-streaming.html
---
+++ app/src/main/java/com/dedykuncoro/videostreaming/MainActivity.java
@@ -0,0 +1,62 @@
+package com.dedykuncoro.videostreaming;
+
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String VIDEO_URL = "https://archive.org/download/Japanese_childrens_song_-_kina_kuri_no_ki_no_shita_de/japanese_childrens_song_-_kina_kuri_no_ki_no_shita_de.mp4";
+
+    ProgressDialog pDialog;
+    VideoView videoView;
+    MediaController mediaController;
+    Uri video;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = (VideoView) findViewById(R.id.video_view);
+
+        videoStream();
+
+    }
+
+    private void videoStream() {
+        // membuat progressbar
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Buffering ...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        try {
+            // Memulai MediaController
+            mediaController = new MediaController(this);
+            mediaController.setAnchorView(videoView);
+            // Video URL
+            video = Uri.parse(VIDEO_URL);
+            videoView.setMediaController(mediaController);
+            videoView.setVideoURI(video);
+            videoView.requestFocus();
+            videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                // Menutup pDialog dan play video
+                public void onPrepared(MediaPlayer mp) {
+                    pDialog.dismiss();
+                    videoView.start();
+                }
+            });
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+}
